ac38aa89719f0ee69eb3c0ff5d9771ee584e0c2a
Joyce Chen <joycechen97@gmail.com>
2019-04-25 21:16:55 -0400

    Added material-ui dep, changed font

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import {Link } from 'react-router-dom';
+import 'typeface-roboto';
 
 class Navbar extends Component {
   render(){
     return (
-      <div>
+      <div className="NavClass">
         <ul>
           <li>
             <Link to="/">Home</Link>
