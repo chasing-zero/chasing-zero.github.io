71980736fa641c73bfd8b8ddd851440f4d32a18a
Joyce Chen <joycechen97@gmail.com>
2019-05-02 03:21:53 -0400

    Landing page now shows SnackBars of items expiring before May 10

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -12,7 +12,9 @@ class Routes extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path='/' component={Landing} />
+        <Route exact path='/' render={
+          () => <Landing inventory={this.props.inventoryState.inventory}/>
+        } />
         <Route exact path='/SignUp' component={SignUp} />
         <Route exact path='/SignIn' component={SignIn} />
         <Route exact path='/ManageInventory' render={
