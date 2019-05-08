28ba3dded4b91fe514f181f9c9c394613de55a82
Joyce Chen <joycechen97@gmail.com>
2019-05-08 13:01:19 -0400

    Removed unnecessary import

--- src/components/InventoryTable.js
+++ src/components/InventoryTable.js
@@ -9,7 +9,6 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
-import SelectInput from '@material-ui/core/Select/SelectInput';
 
 const styles = {
   root: {
